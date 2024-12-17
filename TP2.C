dcaa3717ae8601915ca58866296df3e4dd859899
rahimnacib <rrhoma74@gmail.com>
2024-12-17 02:01:08 +0100

    Update TP2.C

--- TP2.C
+++ TP2.C
@@ -1,35 +1,55 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+char *ChargerChaine(int N);
+int LongueurChaine(char *str);
+void InverserTableau(char Tableau[], char Inverse[], int longueur);
+void ChargerTableau(char *str, char Tableau[]);
+void AfficherTableau(char Tableau[], int longueur);
+
 int main() {
-    int T[10][10];
-    int i , j , save , size;
-    printf("enter size of array : ");
-    scanf("%d",&size);
-    for(i = 0 ; i < size ; i++){
-        for(j = 0 ; j < size ; j++){
-         printf("enter T[%d][%d] : ",i,j);
-         scanf("%d",&T[i][j]);
-        }
-    }
-    printf("original array :\n\n");
-    for(i = 0 ; i < size ; i++){
-        for(j = 0 ; j < size ; j++){
-         printf("\t%d\t|",T[i][j]);
-        }
-        printf("\n\n");
-    }
-    for(i = 1 ; i < size ; i++){
-        for(j = 0 ; j < i ; j++){
-         save = T[i][j];
-         T[i][j] = T[j][i];
-         T[j][i] = save;
-        }
-    }
-    printf("reversed array :\n\n");
-    for(i = 0 ; i < size ; i++){
-        for(j = 0 ; j < size ; j++){
-         printf("\t%d\t|",T[i][j]);
-        }
-        printf("\n\n");
-    }
+    char *str;
+    int n;
+    printf("Veuillez saisir la taille maximale de la chaîne : ");
+    scanf("%d", &n);
+    str = ChargerChaine(n);
+    int longueur = LongueurChaine(str);
+    char Tableau[longueur], Inverse[longueur];
+    ChargerTableau(str, Tableau);
+    AfficherTableau(Tableau, longueur);
+    InverserTableau(Tableau, Inverse, longueur);
+    AfficherTableau(Inverse, longueur);
+    free(str);
     return 0;
+}
+char *ChargerChaine(int N) {
+    char *str = (char *)malloc(N * sizeof(char));
+    printf("Veuillez saisir la chaîne : ");
+    scanf("%s", str);
+    return str;
+}
+int LongueurChaine(char *str) {
+    int longueur_chaine = 0;
+    while (str[longueur_chaine] != '\0') {
+        longueur_chaine++;
+    }
+    return longueur_chaine;
+}
+void InverserTableau(char Tableau[], char Inverse[], int longueur) {
+    for (int i = 0; i < longueur; i++) {
+        Inverse[i] = Tableau[longueur - 1 - i];
+    }
+}
+void ChargerTableau(char *str, char Tableau[]) {
+    int i = 0;
+    while (str[i] != '\0') {
+        Tableau[i] = str[i];
+        i++;
+    }
+    Tableau[i] = '\0';
+}
+void AfficherTableau(char Tableau[], int longueur) {
+    for (int i = 0; i < longueur; i++) {
+        printf("%c", Tableau[i]);
+    }
+    printf("\n");
 }
